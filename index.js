add6ecdc2543a1ee10cd9ffd6647186e9c766ffb
hamza0867 <hamza0867@gmail.com>
2019-01-10 13:00:32 +0100

    Add allow CORS so the app would be able to retrieve data

--- index.js
+++ index.js
@@ -4,13 +4,22 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const nbshopsdb = require("./nearybyshopsdb.js");
 
 const app = express();
-
-app.post("/nbshops/users/new", urlencodedParser, (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    nbshopsdb.registerUser(req.body.userName, req.body.pass).then(newUser => {
-        res.send(JSON.stringify(newUser));
-    });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 })
+    .post("/nbshops/users/new", urlencodedParser, (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        nbshopsdb
+            .registerUser(req.body.userName, req.body.pass)
+            .then(newUser => {
+                res.send(JSON.stringify(newUser));
+            });
+    })
     .post("/nbshops/user", urlencodedParser, (req, res) => {
         res.setHeader("Content-Type", "application/json");
         nbshopsdb.getUser(req.body.userName, req.body.pass).then(user => {
